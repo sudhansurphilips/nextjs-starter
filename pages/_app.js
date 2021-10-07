44f30993093c8077f0b23cefbfda843d6894664d
Sudhansu Sekhar Rana <partner.sudhansu.rana@philips.com>
2021-10-07 19:00:37 +0530

    added bootstrap css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/reset.css';
 import '../styles/main.css';
+import '../styles/global.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
