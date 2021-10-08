b1b8a9d120dde72db514cfd77a56e473bc8070ae
Sudhansu Sekhar Rana <partner.sudhansu.rana@philips.com>
2021-10-08 13:09:52 +0530

    added dynamic products urls

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
+import { getHomeProducts } from './lib/products'
+
 module.exports = {
   trailingSlash: true,
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
   ) {
-    return {
-      "/": { page: "/" },
-      "/products/index": { page: "/products" },
-      "/products/HX9957%2F51": { page: "/products/[id]" },
-      "/products/HY1200%2F08": { page: "/products/[id]" },
-      "/products/S9031%2F90": { page: "/products/[id]" },
-      "/products/HD9654%2F961": { page: "/products/[id]" },
-    };
-  },
-};
+    let routes = {
+      '/': { page: '/' }
+    }
+    const products = await getHomeProducts()
+
+    products.forEach(product => {
+      routes[`/products/${product.id}`] = { page: '/products/[id]' }
+    })
+    return routes
+  }
+}
