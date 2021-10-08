e09fc4869e89f294e3d5dc7a0ac83866eec428e4
Sudhansu Sekhar Rana <partner.sudhansu.rana@philips.com>
2021-10-08 11:06:32 +0530

    added fonts in documents

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,48 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=PT+Sans:300,400,700,800"
             rel="stylesheet"
           />
+           <link
+            rel="preload"
+            href="/fonts/CentraleSans-Regular-webfont.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/centrale-sans-bold.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/centrale-sans-book.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/CentraleSans-Medium.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/CentraleSans-Light.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/CentraleSans-Thin.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/CentraleSans-Xbold.ttf"
+            as="font"
+            crossOrigin=""
+          />
         </Head>
         <body>
           <Main />
