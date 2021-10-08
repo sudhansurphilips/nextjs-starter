baea05b121a057654b341818814a0d0782fe6168
Sudhansu Sekhar Rana <partner.sudhansu.rana@philips.com>
2021-10-08 12:21:07 +0530

    package update

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/react-image-gallery/styles/css/image-gallery.css'
-
-//import '../styles/reset.css';
-//import '../styles/main.css';
 import '../styles/global.css'
 
 // This default export is required in a new `pages/_app.js` file.
