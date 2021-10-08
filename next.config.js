a60da0320897f0c29a2fe7d2c9a14b8bd38e3a19
Sudhansu Sekhar Rana <partner.sudhansu.rana@philips.com>
2021-10-08 13:30:45 +0530

    next-config change 1

--- next.config.js
+++ next.config.js
@@ -1,19 +1,15 @@
-const {getHomeProducts} = require('./lib/products')
-
 module.exports = {
   trailingSlash: true,
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
   ) {
-    let routes = {
-      '/': { page: '/' }
-    }
-    const products = await getHomeProducts()
-
-    products.forEach(product => {
-      routes[`/products/${product.id}`] = { page: '/products/[id]' }
-    })
-    return routes
-  }
-}
+    return {
+      "/": { page: "/" },
+      "/products/HX9957%2F51": { page: "/products/[id]" },
+      "/products/HY1200%2F08": { page: "/products/[id]" },
+      "/products/S9031%2F90": { page: "/products/[id]" },
+      "/products/HD9654%2F961": { page: "/products/[id]" },
+    };
+  },
+};
