c535d17a95237ea18cb377af38c79ac5a847d4bb
Sudhansu Sekhar Rana <partner.sudhansu.rana@philips.com>
2021-10-08 14:13:54 +0530

    next-config change 2

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   trailingSlash: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
+  exportPathMap: function () {
     return {
       "/": { page: "/" },
+      "/products": { page: "/products" },
       "/products/HX9957%2F51": { page: "/products/[id]" },
       "/products/HY1200%2F08": { page: "/products/[id]" },
       "/products/S9031%2F90": { page: "/products/[id]" },
