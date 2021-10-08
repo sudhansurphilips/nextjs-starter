7b8883458a9720c92415a3f6ce0a13afd855c1b0
Sudhansu Sekhar Rana <partner.sudhansu.rana@philips.com>
2021-10-08 12:57:26 +0530

    added products static paths

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   trailingSlash: true,
-  exportPathMap: function () {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
     return {
       "/": { page: "/" },
+      "/products/": { page: "/products/" },
+      "/products/HX9957%2F51": { page: "/products/[id]" },
+      "/products/HY1200%2F08": { page: "/products/[id]" },
+      "/products/S9031%2F90": { page: "/products/[id]" },
+      "/products/HD9654%2F961": { page: "/products/[id]" },
     };
   },
 };
