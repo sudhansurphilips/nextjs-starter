b174ca3cfd1e94b585db3c8c22fb0a556e907d93
Sudhansu Sekhar Rana <partner.sudhansu.rana@philips.com>
2021-10-08 11:09:20 +0530

    removed older css files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/reset.css';
-import '../styles/main.css';
-import '../styles/global.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+//import '../styles/reset.css';
+//import '../styles/main.css';
+import '../styles/global.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
