52e0c00c08c3ae5d04da270275e24b1e7a8d3bcf
Sudhansu Sekhar Rana <partner.sudhansu.rana@philips.com>
2021-10-26 15:23:15 +0530

    commented nextjs config

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@ module.exports = {
     return {
       "/": { page: "/" },
       "/products": { page: "/products" },
-      "/products/HX9957_51": { page: "/products/[id]" },
+      /* "/products/HX9957_51": { page: "/products/[id]" },
       "/products/HY1200_08": { page: "/products/[id]" },
       "/products/S9031_90": { page: "/products/[id]" },
-      "/products/HD9654_96": { page: "/products/[id]" },
+      "/products/HD9654_96": { page: "/products/[id]" }, */
     };
   },
 };
